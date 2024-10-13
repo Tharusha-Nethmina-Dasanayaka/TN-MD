1a9c27cd84043b735e6c5ba1de612a2b8adce3ab
TN_ MASTER_DEV <tharushanethminadasanayaka@gmail.com>
2024-10-13 14:53:06 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
-MONGODB: process.env.MONGODB || "mongodb+srv://kulathungaasitha319:yjHB0DvFfStNfwPS@cluster0.3oijd.mongodb.net/",//enter mongo db url
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=HzhVBARZ#BcAls4moedpaXNukNLsMb7gE9YCQLQrIZxH7xBzfQc0",
+MONGODB: process.env.MONGODB || "mongodb+srv://Tharusha:Tharusha2006N@cluster0.juziauk.mongodb.net/",//enter mongo db url
 };
